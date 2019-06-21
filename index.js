45ec2f0f814abb2aee7789e41d639cb9c7def74f
Damini Varu <damvaru4@gmail.com>
2019-06-21 02:37:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('.target');
+}
+
+function increaseRankBy(n) {
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (var i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n).toString();
+  }
+}
+
+function deepestChild() {
+  const grand = document.getElementById('grand-node');
+  var div = grand.children[0];
+
+  for (var i = 0; i < 3; i++) {
+    div = div.children[0];
+  }
+  console.log(div);
+   return div;
+}
