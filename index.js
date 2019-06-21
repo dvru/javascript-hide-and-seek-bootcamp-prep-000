9f8401a0ff5c45860bb426ee66e715c1c4b76902
Damini Varu <damvaru4@gmail.com>
2019-06-21 02:32:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
-//1
-function getFirstSelector(selector) {
-  document.querySelector(selector)
-  return document.querySelector(selector)
+function getFirstSelector(selector){
+  const lis = document.querySelector(selector);
+  return lis;
+};
+
+function nestedTarget(target){
+  var find = document.getElementById('nested');
+
+  return find;
+};
+
+function deepestChild(){
+  var deep = document.getElementById("grand-node").querySelectorAll('div');
+  return deep[deep.length-1];
+};
+
+function increaseRankBy(n){
+  const lis =document.getElementById('app').querySelectorAll('ul.ranked-list li')
+
+for (let i = 0; i < lis.length; i++) {
+  lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
 }
-
-
-//2
-function nestedTarget() {
-  document.querySelector('div')
-  return document.querySelector('div')
-}
-
-//3  Define a function increaseRankBy(n) that increases the ranks in all of the .ranked-lists by n. (You might need to make use of parseInt()
-function increaseRankBy(n) {
-  n = parseInt(n, 10)
-  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
-  for (let i = 0; i < lis.length; i++) {
-    debugger
-    lis[i].innerHTML = (parseInt(lis[i].innerHTML, 10) + n).toString()
-  }
- return lis
-}
-
-//4
-function deepestChild() {
-  var nodes = document.querySelectorAll('#grand-node div');
-  var mostDeep = nodes[nodes.length - 1]
-  if(!nodes.length) return false;
-  return nodes[nodes.length-1];
+return lis;
 }
