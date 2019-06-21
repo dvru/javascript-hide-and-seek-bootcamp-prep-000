bcfce618d7a4b780d746e825fbbb0aa5852b0331
Damini Varu <damvaru4@gmail.com>
2019-06-21 02:32:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 function getFirstSelector(selector){
   const lis = document.querySelector(selector);
   return lis;
-};
+}
 
 function nestedTarget(target){
   var find = document.getElementById('nested');
 
   return find;
-};
+}
 
 function deepestChild(){
   var deep = document.getElementById("grand-node").querySelectorAll('div');
   return deep[deep.length-1];
-};
+}
 
 function increaseRankBy(n){
   const lis =document.getElementById('app').querySelectorAll('ul.ranked-list li')
